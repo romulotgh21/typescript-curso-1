51f4528234df16bf819cad642df16897a186b65a
Rômulo Nunes <romulotg21@gmail.com>
2023-10-25 09:18:35 -0300

    feat: aprendendo metodos estaticos e parametros adicionais

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -6,7 +6,7 @@ import { DiasDaSemana } from "../enums/dias-da-semana.js";
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView("#negociacoesView");
+        this.negociacoesView = new NegociacoesView("#negociacoesView", true);
         this.mensagemView = new MensagemView("#mensagemView");
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector("#quantidade");
@@ -14,7 +14,8 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        const negociacao = this.criaNegociacao();
+        // const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update("Apenas Negociações dentro de dias uteis");
             return;
@@ -28,13 +29,13 @@ export class NegociacaoController {
         const diaSemana = data.getDay();
         return (diaSemana !== DiasDaSemana.DOMINGO && diaSemana !== DiasDaSemana.SABADO);
     }
-    criaNegociacao() {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
-    }
+    // private criaNegociacao(): Negociacao {
+    //   const exp = /-/g;
+    //   const date = new Date(this.inputData.value.replace(exp, ","));
+    //   const quantidade = parseInt(this.inputQuantidade.value);
+    //   const valor = parseFloat(this.inputValor.value);
+    //   return new Negociacao(date, quantidade, valor);
+    // }
     limparFormulario() {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
