ad04fd0e1687f5dbcfb2297abdd65e177c3ad023
Rômulo Nunes <romulotg21@gmail.com>
2023-10-25 10:03:00 -0300

    feat: strictNullChecks, Remoção de comentários do codigo compilado

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -9,12 +9,11 @@ export class NegociacaoController {
         this.negociacoesView = new NegociacoesView("#negociacoesView", true);
         this.mensagemView = new MensagemView("#mensagemView");
         this.inputData = document.querySelector("#data");
-        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputQuantidade = (document.querySelector("#quantidade"));
         this.inputValor = document.querySelector("#valor");
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        // const negociacao = this.criaNegociacao();
         const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update("Apenas Negociações dentro de dias uteis");
@@ -29,13 +28,6 @@ export class NegociacaoController {
         const diaSemana = data.getDay();
         return (diaSemana !== DiasDaSemana.DOMINGO && diaSemana !== DiasDaSemana.SABADO);
     }
-    // private criaNegociacao(): Negociacao {
-    //   const exp = /-/g;
-    //   const date = new Date(this.inputData.value.replace(exp, ","));
-    //   const quantidade = parseInt(this.inputQuantidade.value);
-    //   const valor = parseFloat(this.inputValor.value);
-    //   return new Negociacao(date, quantidade, valor);
-    // }
     limparFormulario() {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
